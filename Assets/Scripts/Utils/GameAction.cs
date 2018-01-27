6f2eeec949492ee7986564364a742b1d6300bb4a
chaves <alexandregdesign@gmail.com>
2018-01-27 10:42:11 -0200

    add Utils

---
+++ Assets/Scripts/Utils/GameAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class GameAction : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class InteractableEvent : UnityEvent<GameAction> { }
+
+    [SerializeField]
+    protected InteractableEvent _action = new InteractableEvent();
+
+    public void DoAction()
+    {
+        _action.Invoke(this);
+    }
+
+}
---
+++ Assets/Scripts/Utils/Gizmo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gizmo : MonoBehaviour {
+
+    public float radius = .75f;
+    public Color color = Color.yellow;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
