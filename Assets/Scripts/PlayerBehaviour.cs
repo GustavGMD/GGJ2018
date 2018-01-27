58b72edd3cf59cf34ce185ae52c81082a6f183b7
GustavoMarangon <gromanmarangon@gmail.com>
2018-01-27 15:21:04 -0200

    Blobs roubando de Blobs
    Tem um bug de escala em que as bolinhas as vezes ficam gigantes

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -155,7 +155,7 @@ public class PlayerBehaviour : MonoBehaviour {
             __tempHook.SetActive(false);
         };
         __tempHook.GetComponent<HookBehaviour>().onEnemyCollision += delegate(PlayerBehaviour p_other)
-        {            
+        {
             if (p_other != this)
             {
                 //atingiu um player isolado
@@ -174,45 +174,70 @@ public class PlayerBehaviour : MonoBehaviour {
 
                     //Play animations
                     BlobJoinAnimation(p_other.gameObject);
-                    GrowingAnimation();
+                    GrowingAnimation(1f);
                 }
                 //atingiu um Blob
                 else
                 {
                     //A blob atingida desprende um de seus blobs: chamamos um método nela que retorna o objeto a ser manipulado, e um outro que retorna o input
+                    //Pega a referência das coisas do outro
+                    GameObject __tempObject = p_other.RemoveParticleFromBlob();
+                    char __tempInput = p_other.RemoveInputFromBlob();
+                    capturedPlayersStack.Push(__tempObject);
+                    capturedInputsStack.Push(__tempInput);
+
+                    //chama as animações
+                    BlobJoinAnimation(__tempObject);
+                    GrowingAnimation(1);
+                    p_other.GrowingAnimation(0);
                 }
-            }
+
+                //desativa pra ele não colidir com mais nada
+                __tempHook.SetActive(false);
+            }            
         };
     }
     public void BlobJoinAnimation(GameObject p_target)
     {
+        p_target.GetComponent<Renderer>().enabled = true;
+        p_target.transform.SetParent(null);
+        p_target.transform.localScale = Vector3.one * 3;
         p_target.transform.SetParent(transform);
         PotaTween __pTween = p_target.GetComponent<PotaTween>();
         __pTween.Reset();
         __pTween.SetPosition(p_target.transform.localPosition, Vector2.zero, true);
         __pTween.SetEaseEquation(Ease.Equation.InSine);
+        __pTween.Play(delegate 
+        {
+            p_target.GetComponent<Renderer>().enabled = false;
+        });
+    }
+
+    public void GrowingAnimation(float p_delay)
+    {        
+        if (GetComponent<PotaTween>() == null) PotaTween.Create(gameObject);
+        PotaTween __pTween = GetComponent<PotaTween>();
+        __pTween.SetScale(transform.localScale, Vector3.one*(capturedPlayersStack.Count+1)*3);
+        __pTween.SetEaseEquation(Ease.Equation.OutElastic);
+        __pTween.SetDelay(p_delay);
         __pTween.Play();
     }
 
-    public void GrowingAnimation()
+    public GameObject RemoveParticleFromBlob()
     {
-        PotaTween pTween = PotaTween.Create(gameObject, 2);
-        pTween.SetScale(transform.localScale, Vector3.one*(capturedPlayersStack.Count+1)*2);
-        pTween.SetEaseEquation(Ease.Equation.OutInElastic);
-        pTween.SetDelay(1);
-        pTween.Play();
+        return capturedPlayersStack.Pop();
     }
-
-    //public GameObject RemoveParticleFromBlob()
-    //{
-    //
-    //}
+    public char RemoveInputFromBlob()
+    {
+        return capturedInputsStack.Pop();
+    }
 
     public void CleanCapturedPlayer(GameObject p_captured)
     {
         Destroy(p_captured.GetComponent<PlayerBehaviour>());
         Destroy(p_captured.GetComponent<Rigidbody2D>());
         Destroy(p_captured.GetComponent<Collider2D>());
+        p_captured.GetComponent<Renderer>().enabled = false;
         Transform[] __children = p_captured.transform.GetComponentsInChildren<Transform>();
         for (int i = 0; i < __children.Length; i++)
         {
